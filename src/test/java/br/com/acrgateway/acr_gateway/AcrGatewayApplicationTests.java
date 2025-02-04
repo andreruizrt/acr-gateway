d602d020af5fe2a4725b9903ebdc228a92076623
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2025-02-04 00:33:12 -0300

    inicia projeto spring gateway

---
+++ src/test/java/br/com/acrgateway/acr_gateway/AcrGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acrgateway.acr_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
